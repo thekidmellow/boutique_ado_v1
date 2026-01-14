a229022dfdd00970062e399e5acedb8d2179f155
thekidmellow <thekidmellow@gmail.com>
2026-01-14 10:39:32 +0100

    Completed home page header and css

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -137,4 +137,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("", include("home.urls")),
 ]
-
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
